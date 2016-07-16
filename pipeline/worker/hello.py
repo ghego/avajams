365070a517c27c996005e7864742aeae99353aac
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2016-07-16 23:17:17 +0000

    fix download

--- pipeline/worker/hello.py
+++ pipeline/worker/hello.py
@@ -12,10 +12,10 @@ url = payload['url']
 
 print "url:", url
 
-#downloaded_file_path = convert_embed_url_and_download(url)
-#print "download file path:", downloaded_file_path 
-downloaded_file_path = "test_video.mp4"
+downloaded_file_path = convert_embed_url_and_download(url)
+print "download file path:", downloaded_file_path 
+#downloaded_file_path = "test_video.mp4"
 upload_s3(downloaded_file_path)
 
-#images = extract_images("./test_video.mp4")
+images = extract_images(downloaded_file_path)
 print 'Here is the task_id: %s' %  IronWorker.task_id()
