6b1f2a9c1b7a54a4949008ee01fe2f9aec3caa1d
Aaron Wertman <awertman@chegg.com>
2016-07-16 14:00:57 -0700

    updated video retrieval for multiple index selection

--- lib/tasks/get_video_download_uris.py
+++ lib/tasks/get_video_download_uris.py
@@ -7,7 +7,7 @@ from urlparse import parse_qs, urlparse
 import argparse
 
 def convert_embed_to_watch(url):
-  components = r['urls'].replace('embed', 'watch').split("/")
+  components = url.replace('embed', 'watch').split("/")
   root = "/".join(components[:-1])
   video_id = components[-1]
   video_url = root +"?v="+video_id
@@ -25,7 +25,8 @@ def download_file(url, video_id):
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument("--data", help="Input data file", type=str)
-  parser.add_argument("--index", help="Test a saved classifier", type=int, default=0)
+  parser.add_argument("--startindex", help="Test a saved classifier", type=int, default=0)
+  parser.add_argument("--endindex", help="Test a saved classifier", type=int, default=-1)
   args = parser.parse_args()
 
   file_path = os.path.join(os.environ['PROJECT_ROOT'], args.data)
@@ -42,13 +43,13 @@ if __name__ == '__main__':
     writer.writeheader()
   
     out_data = []
-    for r in rows[args.index:]:
+    for r in rows[args.startindex:args.endindex]:
       video_url, video_id = convert_embed_to_watch(r['urls'])
 
       data = 'mediaurl=' + video_url +'&filename=&filetype=&format=&audiovol=0&audiochannel=2&audiobr=128&videobr=224&videores=352x288&videoaspect=&customres=320x240&timefrom-start=1&timeto-end=1&id3-artist=&id3-title=&id3-album=ClipConverter.cc&auto=0&hash=&image=&org-filename=&videoid=&pattern=&server=&serverinterface=&service=&ref=&lang=en&client_urlmap=none&ipv6=false&addon_urlmap=&cookie=&addon_cookie=&addon_title=&ablock=1&clientside=0&addon_page=none&verify=&addon_browser=&addon_version='
       response = WebRequest.get(uri, headers=headers, data=data, timeout=30)
       if response.get('redirect'):
-        raise Exception("|ERROR| Captcha validation requested |{}|".format(response))
+        raise Exception("Captcha validation requested |{}|".format(response))
       
       video_items = sorted([r for r in response['url'] if r['filetype'] == 'MP4' and r.get('checked')], key=lambda x: x['size'])
       
