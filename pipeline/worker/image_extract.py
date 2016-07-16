30c03aca4453ca28df53b264d72d0a431d286817
Francesco Mosconi <git@mosconi.me>
2016-07-16 14:08:57 -0700

    python only extractor + sound extractor

--- pipeline/worker/image_extract.py
+++ pipeline/worker/image_extract.py
@@ -1,13 +1,30 @@
+# http://ffmpeg.org/ffmpeg.html#Video-Options
+# https://trac.ffmpeg.org/wiki/Scaling%20(resizing)%20with%20ffmpeg
 import subprocess
 import logging
 import re
-
+import os
 logger = logging.getLogger('avajams.image_extract')
 
+NIMAGES = 200
+SCALE = 320
 
 def extract_images(video_file_name):
     logger.info("extracting: %s", video_file_name)
-    subprocess.call("./image_extract.sh ./%s" % video_file_name, shell = True)
+    output_path = video_file_name.replace('.mp4', '')
+    for i in xrange(1, NIMAGES):
+        command = """ffmpeg -y -accurate_seek -ss `echo {i}*1.0 | \
+                     bc` -i {video} -frames:v 1 -vf scale={scale}:-1 \
+                     {folder}/{i}.jpg""".format(i=i,
+                                                scale=SCALE,
+                                                video=video_file_name,
+                                                folder=output_path)
+        subprocess.call(command, shell=True)
+
+    command = """ffmpeg -y -i {video} -b:a 192K -vn {folder}/audio.mp3""".format(i=i,
+                                               video=video_file_name,
+                                               folder=output_path)
+    subprocess.call(command, shell=True)
     logger.info("done!")
 
 if __name__ == '__main__':
--- pipeline/worker/image_extract.sh
+++
@@ -1,7 +0,0 @@
-# http://ffmpeg.org/ffmpeg.html#Video-Options
-# https://trac.ffmpeg.org/wiki/Scaling%20(resizing)%20with%20ffmpeg
-
-VIDEO=$1
-FOLDER=${2:-"${VIDEO/\.[a-z]*/}"}
-mkdir -p $FOLDER
-time for i in {0..200} ; do ffmpeg -y -accurate_seek -ss `echo $i*1.0 | bc` -i $VIDEO  -frames:v 1 -vf scale=320:240 $FOLDER/$i.jpg ; done
