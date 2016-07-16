2a0abcd2ccfec14ebef71b579b755ef1c5c3857f
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2016-07-16 21:07:34 +0000

    upload csv file

--- pipeline/worker/enqueue.py
+++ pipeline/worker/enqueue.py
@@ -1,7 +1,13 @@
 # Here is how to queue tasks from Python.
 from iron_worker import *
 
-worker = IronWorker(project_id=your_project_id, token=your_project_token)
-task = worker.queue(code_name="hello", payload={"fruits": ["apples", "oranges", "bananas"], "best_song_ever": "Call Me Maybe"})
+import csv
+dr = csv.DictReader(open("input.csv"))
 
-print task.id
+worker = IronWorker(project_id=your_project_id, token=your_project_token)
+
+for row in dr:
+	url = row['urls']	
+	task = worker.queue(code_name="hello", payload={"url": url })
+	print url
+	print task.id
