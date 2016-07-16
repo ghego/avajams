dc774c2134c8e4c184c51aaca6fefe1b568b0838
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2016-07-16 21:13:53 +0000

    fix

--- pipeline/worker/hello.py
+++ pipeline/worker/hello.py
@@ -2,7 +2,11 @@ import sys
 sys.path.append("packages")
 import iron_mq
 from iron_worker import *
+from video_download import *
 
+
+downloaded_file_path = convert_embed_url_and_download('https://www.youtube.com/embed/fDrTbLXHKu8')
+print downloaded_file_path 
 print 'Hello %s' %  IronWorker.payload()['name']
 print
 print 'Here is the payload: %s' %  IronWorker.payload()
