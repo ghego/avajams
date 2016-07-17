ab03ad03df35c64814a5d1d088ba3c0177582ddb
seppemans <sep@seplog.net>
2016-07-17 00:19:16 -0700

    Fix of video_score_avg

--- train/load_features.py
+++ train/load_features.py
@@ -5,6 +5,7 @@ Loading softmax.np and meta.p data from features.extract
 
 import numpy as np
 import pickle
+from collections import defaultdict
 
 SOFTMAX_SIZE = 1008
 VIDEO_SIZE = 1000
@@ -25,31 +26,46 @@ def load_from_video(vs, vmeta, index):
     print('Video: ', vmeta[index], sum(vs[index,:]))    
     return
 
+def combine_vectors(vecs):
+    return np.sum(vecs, axis=0)/vecs.shape[0]
+
 def video_score_avg(fp, meta):
-    #for idx, (video, filename) in enumerate(meta)
-    
-    video_score = np.zeros((VIDEO_SIZE, SOFTMAX_SIZE))
-    video_meta = [] 
-    
-    image_count = 0
-    video_index = 0
-    video_name = ''
-    score = np.zeros((1, SOFTMAX_SIZE))
-    
-    for it in range(len(meta)):
-        file_dir, file_name = meta[it]
-        score[0,:] = score[0,:] + fp[it,:]
-        image_count = image_count + 1
-        
-        if file_dir != video_name:
-            video_score[video_index,:] = score[0,:]/image_count
-            video_meta.append(file_dir)
-            
-            image_count = 0
-            video_index = video_index + 1
-            video_name = file_dir
-                
+    video_to_index = defaultdict(list)
+    for idx, (video, filename) in enumerate(meta):
+        video_to_index[video].append(idx)
+
+    video_meta = video_to_index.keys()
+    video_score = np.zeros((len(video_meta), SOFTMAX_SIZE))
+    for i, video in enumerate(video_meta):
+        combined_vec = combine_vectors(fp[video_to_index[video]])
+        video_score[i] = combined_vec
+
     return video_score, video_meta
+
+# def video_score_avg(fp, meta):
+#     video_score = np.zeros((VIDEO_SIZE, SOFTMAX_SIZE))
+#     video_meta = []
+    
+#     image_count = 0
+#     video_index = 0
+#     video_name = ''
+#     score = np.zeros((1, SOFTMAX_SIZE))
+    
+#     for it in range(len(meta)):
+#         file_dir, _ = meta[it]
+#         score[0,:] = score[0,:] + fp[it,:]
+#         image_count = image_count + 1
+        
+#         if file_dir != video_name:
+#             print score[0,:]
+#             video_score[video_index,:] = score[0,:]/image_count
+#             video_meta.append(file_dir)
+            
+#             image_count = 0
+#             video_index = video_index + 1
+#             video_name = file_dir
+                
+#     return video_score, video_meta
 
 def main(argv):
         
